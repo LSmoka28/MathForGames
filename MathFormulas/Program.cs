7463581f83c5b43f9ad99364f1ba4c60833e0aa9
Larry <larry3rdh@gmail.com>
2020-09-30 18:04:49 -0700

    Add problems a-c, for math formulas

--- MathFormulas/Program.cs
+++ MathFormulas/Program.cs
@@ -8,8 +8,62 @@ namespace MathFormulas
 {
     class Program
     {
+        static float ExerciseA(float x)
+        {
+            float result = x * x + 2 * x - 7;
+            return result;
+        }
+
+        static void ExerciseB()
+        {
+            int a, b, c;
+            double d, x1, x2;
+
+            Console.Write("Input the value of a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value of b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the value of c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            d = b * b - 4 * a * c;
+            if (d == 0)
+            {
+                Console.Write("Both roots are equal.\n");
+                x1 = -b / (2.0 * a);
+                x2 = x1;
+                
+            }
+            else if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+                Console.WriteLine($"Root 1: {x1}\n" +
+                    $"Root 2: {x2}\n");
+            }
+            else
+            {
+                Console.WriteLine($"No solution");
+            }
+            
+        }
+
+        static int ExerciseC(int s, int e, int t)
+        {
+            int result = s + (t * (e - s));
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(ExerciseA(7));
+
+            ExerciseB();
+
+            Console.WriteLine(ExerciseC(3, 5, 6));
+            Console.ReadKey();
+
         }
     }
 }

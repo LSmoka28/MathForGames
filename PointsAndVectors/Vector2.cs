83bf206a7961c0c827c63ff69f38e75334e2f8d4
Larry <larry3rdh@gmail.com>
2020-10-01 15:46:14 -0700

    Add PointsAndVectors tutorial

---
+++ PointsAndVectors/Vector2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointsAndVectors
+{
+    class Vector2
+    {
+        // fields for storing values x and y 
+        public float x;
+        public float y;
+        // default constructor
+
+        public Vector2()
+        {
+            // left intentionally blank
+        }
+
+        // will initiaize the x and y fields
+        public Vector2(float xArg, float yArg)
+        {
+            x = xArg;
+            y = yArg;
+        }
+
+        // defines an operator for adding two Vector2s
+        public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2(lhs.x + rhs.x, lhs.y + rhs.y);
+        }
+
+        // defines operator for subtracting two Vector2s
+        public static Vector2 operator -(Vector2 lhs, Vector2 rhs)
+        {
+            return new Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
+        }
+
+        // defines op for multiplying the first Vector2 by a float number
+        public static Vector2 operator *(Vector2 lhs, float multiplyBy)
+        {
+            return new Vector2(lhs.x * multiplyBy, lhs.y * multiplyBy);
+        }
+
+        // defines op for multiplying the second Vector2 by a float number
+        public static Vector2 operator *(float multiplyBy, Vector2 rhs)
+        {
+            return new Vector2(rhs.x * multiplyBy, rhs.y * multiplyBy);
+        }
+
+        // defines op for diving the first Vector2 by a float number
+        public static Vector2 operator /(Vector2 lhs, float divideBy)
+        {
+            return new Vector2(lhs.x / divideBy, lhs.y / divideBy);
+        }
+        
+        // defines op for diving the second Vector2 by a float number
+        public static Vector2 operator /(float divideBy, Vector2 rhs)
+        {
+            return new Vector2(rhs.x / divideBy, rhs.y / divideBy);
+        }
+    }
+}
---
+++ PointsAndVectors/Vector3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointsAndVectors
+{
+    class Vector3
+    {
+        // fields for storing values x, y, and z 
+        public float x;
+        public float y;
+        public float z;
+
+        // default constructor
+        public Vector3()
+        {
+            // left intentionally blank
+        }
+
+        // will initiaize the x, y, and z fields
+        public Vector3(float xArg, float yArg, float zArg)
+        {
+            x = xArg;
+            y = yArg;
+            z = zArg;
+        }
+
+        // defines an operator for adding two Vector3s
+        public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+        }
+
+        // defines an operator for subtracting two Vector3s
+        public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+        }
+
+        // defines op for multiplying the first Vector3 by a float number
+        public static Vector3 operator *(Vector3 lhs, float multiplyBy)
+        {
+            return new Vector3(lhs.x * multiplyBy, lhs.y * multiplyBy, lhs.z * multiplyBy);
+        }
+
+        // defines op for multiplying the second Vector3 by a float number
+        public static Vector3 operator *(float multiplyBy, Vector3 rhs)
+        {
+            return new Vector3(rhs.x * multiplyBy, rhs.y * multiplyBy, rhs.z * multiplyBy);
+        }
+
+        // defines op for diving the first Vector3 by a float number
+        public static Vector3 operator /(Vector3 lhs, float divideBy)
+        {
+            return new Vector3(lhs.x / divideBy, lhs.y / divideBy, lhs.z / divideBy);
+        }
+
+        // defines op for diving the second Vector3 by a float number
+        public static Vector3 operator /(float divideBy, Vector3 rhs)
+        {
+            return new Vector3(rhs.x / divideBy, rhs.y / divideBy, rhs.z / divideBy);
+        }
+    }
+}
